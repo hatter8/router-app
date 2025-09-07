b79464829d2f176f21de73e0754dd9934021f97a
hatter8 <madda45@live.co.uk>
2025-09-07 10:42:37 +0100

    Revert "Update About.js"
    This reverts commit b02a5121cbae8e1204cbf6be793bf6456c4585ba.

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,17 @@ export default function About() {
                 <div className="">
                     <h2>About Page</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat unde rem fugiat, repudiandae soluta molestias harum? Numquam a ipsam ex nulla, quidem quod tempore, praesentium exercitationem doloremque maiores non nam!</p>
+<<<<<<< Updated upstream
+                                    
+=======
                     <div>
                         <article>
                             <h3>Article heading</h3> 
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sequi?</p>   
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sequi?</p> 
+                            <button>Learn more</button>  
                         </article>    
                     </div>                
+>>>>>>> Stashed changes
                 </div>
             </>
         )
